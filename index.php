59c2e053a2adc1e0193290d3304fa30ceaa5a4ce
fatihemree <fatihemree.klm@gmail.com>
2019-02-17 13:28:33 +0300

    nav finish fix:responsive / class:navbar-brand add

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>blog</title>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:800" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/ajax.js"></script>
+</head>
+<body>
+	
+	<nav>
+		<div class="nav-wrapper navbar ">
+			<div class="sideNavMenu"><i class="material-icons">clear_all</i></div>
+			<a href="#" ><i class="fas fa-home"></i></a>
+			<div class="navSearchD ">
+				<input type="text" placeholder="Arama Yapın" class="navSearch" name="navSearch">
+			</div>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+			<button class="btn waves-effect waves-light" type="submit" name="action">Giriş Yap</button>
+</ul>
+				
+
+
+		</div>
+	</nav>
+
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<script src="js/login.js"></script>
+	<script src="js/js.js"></script>
+
+
+</body>
+</html>
